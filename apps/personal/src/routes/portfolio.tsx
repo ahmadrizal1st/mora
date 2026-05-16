caaf7553e87685460007034c69820c85e37a86f7
Ahmad Rizal <ahmaddrizalul@gmail.com>
2026-05-16 19:17:48 +0700

    chore: update Tabler Icons library and project dependencies

--- apps/personal/src/routes/portfolio.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute, lazyRouteComponent } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/portfolio')({
-  component: lazyRouteComponent(() => import('@/features/wealth/pages/WealthPage').then(m => ({ default: m.WealthPage }))),
-})
